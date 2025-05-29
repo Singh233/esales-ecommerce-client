0c759512e6f4168ccd156b10a875b6f34d0c8666
Sanambir Singh <Sanambir123@gmail.com>
2025-05-29 14:26:27 +0530

    feat: add Toaster and NextTopLoader components for enhanced UI feedback

--- src/lib/providers/providers.jsx
+++ src/lib/providers/providers.jsx
@@ -1,11 +1,22 @@
 import ReduxProvider from "../redux/provider";
 import ReactQueryProvider from "./react-query";
+import { Toaster } from "sonner";
+import NextTopLoader from "nextjs-toploader";
 
 // Wrapper component to provide all providers to the app
 function Providers({ children }) {
   return (
     <>
       <ReactQueryProvider>
+        <Toaster
+          richColors
+          toastOptions={{
+            className: "rounded-lg shadow-lg",
+          }}
+          position="top-right"
+          closeButton
+        />
+        <NextTopLoader color="#7E22CE" showSpinner={false} height={4} />
         <ReduxProvider>{children}</ReduxProvider>
       </ReactQueryProvider>
     </>
